77c35e3aa192e2f634bcb041e3d7f2ea006d3395
lt-name <572413378@qq.com>
2020-10-06 11:09:35 +0800

    improve

--- src/main/java/me/CookieLuck/Events.java
+++ src/main/java/me/CookieLuck/Events.java
@@ -16,8 +16,7 @@ import cn.nukkit.utils.TextFormat;
 
 public class Events implements Listener {
 
-	GameLevel plugin;
-	Main main;
+	private final Main main;
 
 	Events(Main main) {
 		this.main = main;
@@ -25,30 +24,25 @@ public class Events implements Listener {
 
 	@EventHandler
 	public void itemdrop(PlayerDropItemEvent e) {
-		if(GameLevel.getGameLevelByWorld(e.getPlayer().getLevel().getName()) != null){
-			plugin = GameLevel.getGameLevelByWorld(e.getPlayer().getLevel().getName());
-			if (plugin.configuring) {
-				e.setCancelled(true);
-			}
-			if(plugin.waiting){
+		GameLevel gameLevel = GameLevel.getGameLevelByWorld(e.getPlayer().getLevel().getName());
+		if(gameLevel != null){
+			if (gameLevel.configuring || gameLevel.waiting) {
 				e.setCancelled(true);
 			}
 		}
-
-
 	}
 
 	@EventHandler
 	public void irse(PlayerQuitEvent e) {
 		Player p = e.getPlayer();
-		if(GameLevel.getGameLevelByWorld(e.getPlayer().getLevel().getName()) != null){
-			plugin = GameLevel.getGameLevelByWorld(((p.getLevel().getName())));
-			if(!plugin.waiting && !plugin.configuring && !plugin.building){
-				plugin.die(p);
+		GameLevel gameLevel = GameLevel.getGameLevelByWorld(((p.getLevel().getName())));
+		if(gameLevel != null){
+			if(!gameLevel.waiting && !gameLevel.configuring && !gameLevel.building){
+				gameLevel.die(p);
 				return;
 			}
-			if(plugin.waiting){
-				plugin.leave(e.getPlayer());
+			if(gameLevel.waiting){
+				gameLevel.leave(e.getPlayer());
 			}
 
 		}
@@ -93,22 +87,20 @@ public class Events implements Listener {
 
 	@EventHandler
 	public void damaged(EntityDamageEvent e) {
-		Entity pe = e.getEntity();
-		if (GameLevel.getGameLevelByWorld(((pe.getLevel().getName()))) == null) {
-			return;
-		}
-		plugin = GameLevel.getGameLevelByWorld(((pe.getLevel().getName())));
 		if (e.getEntity() instanceof Player) {
-
 			Player p = (Player) e.getEntity();
+			GameLevel gameLevel = GameLevel.getGameLevelByWorld(((p.getLevel().getName())));
+			if (gameLevel == null) {
+				return;
+			}
 
-			if (plugin.invulnerable) {
+			if (gameLevel.invulnerable) {
 				e.setCancelled(true);
 			}
 
 			if (e.getDamage() >= p.getHealth()) {
 				e.setCancelled(true);
-				plugin.die(p);
+				gameLevel.die(p);
 
 			}
 		}
@@ -117,51 +109,43 @@ public class Events implements Listener {
 
 	@EventHandler
 	public void onInteract(PlayerInteractEvent e) {
-		if(GameLevel.getGameLevelByWorld(e.getPlayer().getLevel().getName()) == null){
+		GameLevel gameLevel = GameLevel.getGameLevelByWorld(e.getPlayer().getLevel().getName());
+		if(gameLevel == null){
 			return;
 		}
-		plugin = GameLevel.getGameLevelByWorld(e.getPlayer().getLevel().getName());
 
-
-		
 		Player p = e.getPlayer();
 		if (e.getItem().getCustomName().equals(TextFormat.DARK_GREEN + "BACK TO LOBBY")) {
-			plugin.leave(p);
+			gameLevel.leave(p);
 		}
 
-		if (plugin.configuring) {
-			if (e.getAction().equals(Action.RIGHT_CLICK_AIR)) {
-
+		if (gameLevel.configuring) {
+			if (e.getAction() == Action.RIGHT_CLICK_AIR) {
 				if (e.getItem().getCustomName().equals(TextFormat.DARK_GREEN + "WAND")) {
-
 					p.getInventory().clearAll();
-					if (plugin.spawnList.size() != plugin.maxPlayers) {
-
-						plugin.spawnList.add(new Spawn(p.getTargetBlock(1000).getX(), p.getTargetBlock(1000).getY() + 1,
+					p.getUIInventory().clearAll();
+					if (gameLevel.spawnList.size() != gameLevel.maxPlayers) {
+						gameLevel.spawnList.add(new Spawn(p.getTargetBlock(1000).getX(),
+								p.getTargetBlock(1000).getY() + 1,
 								p.getTargetBlock(1000).getZ()));
 						p.getLevel().addSound(p.getPosition(), Sound.BUBBLE_POP, 1, (float) 0.6);
 					}
 
-				}
-			}
-
-			if (e.getAction().equals(Action.RIGHT_CLICK_AIR)) {
-
-				if (e.getItem().getCustomName().equals(TextFormat.DARK_RED + "BACK")) {
+				}else if (e.getItem().getCustomName().equals(TextFormat.DARK_RED + "BACK")) {
 					p.getInventory().clearAll();
-					if (plugin.spawnList.size() > 0) {
+					p.getUIInventory().clearAll();
+					if (gameLevel.spawnList.size() > 0) {
 						p.getLevel().addSound(p.getPosition(), Sound.BLOCK_TURTLE_EGG_CRACK, 1, (float) 0.6);
-						plugin.spawnList.remove(plugin.spawnList.size() - 1);
+						gameLevel.spawnList.remove(gameLevel.spawnList.size() - 1);
 					}
-
 				}
 			}
 			if (e.getItem().getCustomName().equals(TextFormat.DARK_GREEN + "DONE")) {
 				p.getInventory().clearAll();
 				p.getLevel().addSound(p.getPosition(), Sound.RANDOM_LEVELUP, 1, (float) 0.8);
-				plugin.configuring = false;
-				plugin.emptySpawns = false;
-				plugin.waiting = true;
+				gameLevel.configuring = false;
+				gameLevel.emptySpawns = false;
+				gameLevel.waiting = true;
 				
 				p.sendActionBar(TextFormat.DARK_GREEN + "" + TextFormat.BOLD + "NICE!");
 				main.saveGameLevels();
