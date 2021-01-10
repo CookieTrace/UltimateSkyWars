5e1742cb21311fc634ce67cc5cf50607d2eef16a
lt_name <572413378@qq.com>
2021-01-10 16:35:55 +0800

    update Language

--- src/main/java/me/CookieLuck/utils/Language.java
+++ src/main/java/me/CookieLuck/utils/Language.java
@@ -3,6 +3,7 @@ package me.CookieLuck.utils;
 import cn.nukkit.utils.Config;
 
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author lt_name
@@ -10,6 +11,7 @@ import java.util.Objects;
 public class Language {
 
     private final Config config;
+    private final ConcurrentHashMap<String, String> cache = new ConcurrentHashMap<>();
 
     public Language(Config config) {
         this.config = config;
@@ -20,14 +22,33 @@ public class Language {
     }
 
     public String translateString(String key, Object... params) {
-        String string = this.config.getString(key, "§c Language reading error! ");
+        String string = this.cache.computeIfAbsent(key,
+                str -> this.config.getString(key, "§c Language reading error!"));
         if (params != null && params.length > 0) {
             for (int i = 1; i < params.length + 1; i++) {
-                string = string.replace("%" + i, Objects.toString(params[i-1]));
+                string = string.replace("%" + i + "%", Objects.toString(params[i-1]));
             }
         }
         return string;
     }
 
+    public void update(Config newConfig) {
+        for (String key : this.config.getKeys()) {
+            if (newConfig.getKeys().contains(key)) {
+                this.cache.put(key, this.config.getString(key, "§c Language reading error!"));
+            }else {
+                this.cache.remove(key);
+                this.config.remove(key);
+            }
+        }
+        for (String key : newConfig.getKeys()) {
+            if (!this.cache.containsKey(key)) {
+                String string = newConfig.getString(key, "§c Language reading error!");
+                this.config.set(key, string);
+                this.cache.put(key, string);
+            }
+        }
+        this.config.save(true);
+    }
 
 }
